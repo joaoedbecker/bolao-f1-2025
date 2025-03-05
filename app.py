1b3424f115b674e19ce8b9a126b3f37eb6139729
João Becker <joaobecker@Joaos-Air.home>
2025-03-05 18:26:20 +0000

    Configurando porta dinâmica para Railway

--- app.py
+++ app.py
@@ -311,4 +311,5 @@ def calendario():
 if __name__ == '__main__':
     init_db()
     criar_usuarios()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
